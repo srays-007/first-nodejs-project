c82e6dfc2f0380367cca833d0a7a46899a11634b
srays-007 <shibamr527@gmail.com>
2026-09-25 15:51:03 +0530

    feat:content change

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.writeFileSync("test.txt","Hellow world!!!!");
+fs.writeFileSync("test.txt","Hellow world my name is shibam!!!!");
 
 const data = fs.readFileSync('test.txt','utf8');
 console.log(data);
